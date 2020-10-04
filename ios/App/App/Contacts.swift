890502afb2ad2881797e0558366780f7ad1dbe38
Vy Le <lethaovy2001@gmail.com>
2020-10-04 13:23:05 -0500

    add requestAccess Contact plugin

--- ios/App/App/Contacts.swift
+++ ios/App/App/Contacts.swift
@@ -7,32 +7,33 @@ public class Contacts : CAPPlugin {
     // MARK: - Properties
     private let contactStore = CNContactStore()
     private let keysToFetch = [CNContactGivenNameKey,
-                       CNContactFamilyNameKey,
-                       CNContactPhoneNumbersKey,
-                       CNContactEmailAddressesKey] as [CNKeyDescriptor]
+                               CNContactFamilyNameKey,
+                               CNContactPhoneNumbersKey,
+                               CNContactEmailAddressesKey] as [CNKeyDescriptor]
     private var contacts = [Any]()
     
+    // MARK: - Plugin Methods
     @objc func getAll(_ call: CAPPluginCall) {
-        requestContactAccess { result in
-            switch result {
-            case .success(_):
-                //self.fetchAllContacts(call)
-                self.getFilteredContacts(call)
-            case .failure(let error):
-                call.reject(error.localizedDescription)
+        do {
+            let request = CNContactFetchRequest(keysToFetch: self.keysToFetch)
+            try self.contactStore.enumerateContacts(with: request) { contact, stop in
+                self.addToListOfContacts(contact: contact)
             }
+            call.success([
+                "contacts": contacts
+            ])
+        } catch {
+            call.reject("Error fetching contacts \(error)")
         }
     }
     
     @objc func getFilteredContacts(_ call: CAPPluginCall) {
-        let firstName = call.options["firstName"] as? String ?? ""
+        let predicate = getPredicate(from: call)
         do {
-            let predicate = CNContact.predicateForContacts(matchingName: firstName)
             let data = try contactStore.unifiedContacts(matching: predicate, keysToFetch: keysToFetch)
             for contact in data {
                 addToListOfContacts(contact: contact)
             }
-            print(contacts)
             call.success([
                 "contacts": contacts
             ])
@@ -41,40 +42,49 @@ public class Contacts : CAPPlugin {
         }
     }
     
-    // MARK: - Private Methods
-    private func fetchAllContacts(_ call: CAPPluginCall) {
-        do {
-        let request = CNContactFetchRequest(keysToFetch: self.keysToFetch)
-        try self.contactStore.enumerateContacts(with: request) { contact, stop in
-            self.addToListOfContacts(contact: contact)
-        }
-        print(contacts)
-        call.success([
-            "contacts": contacts
-        ])
-        } catch {
-            call.reject("Error fetching contacts \(error)")
+    @objc func requestAccess(_ call: CAPPluginCall) {
+        contactStore.requestAccess(for: .contacts) { granted, error in
+            if granted {
+                call.success()
+            }
+            if let error = error {
+                call.reject(error.localizedDescription)
+            }
         }
     }
     
+    // MARK: - Private Methods
     private func addToListOfContacts(contact: CNContact) {
+        var phoneNumbers = [String]()
+        var emailAddresses = [String]()
+        
+        for phoneNumber in contact.phoneNumbers {
+            phoneNumbers.append(phoneNumber.value.stringValue)
+        }
+        for email in contact.emailAddresses {
+            emailAddresses.append(email.value as String)
+        }
+        
         contacts.append([
             "firstName": contact.givenName,
             "lastName": contact.familyName,
-            "phoneNumbers": [contact.phoneNumbers.first?.value.stringValue],
-            "emailAddresses": [contact.emailAddresses.first?.value]
+            "phoneNumbers": phoneNumbers,
+            "emailAddresses": emailAddresses
         ])
     }
     
-    private func requestContactAccess(completionHandler: @escaping (Result<Bool, Error>) -> Void) {
-        contactStore.requestAccess(for: .contacts) { granted, error in
-            if granted {
-                completionHandler(.success(true))
-            }
-            if let error = error {
-                completionHandler(.failure(error))
-            }
+    private func getPredicate(from call: CAPPluginCall) -> NSPredicate {
+        var predicate = CNContact.predicateForContacts(matchingName: "")
+        if let firstName = call.options["firstName"] as? String {
+            predicate = CNContact.predicateForContacts(matchingName: firstName)
         }
+        if let phoneNumber = call.options["phoneNumber"] as? CNPhoneNumber {
+            predicate = CNContact.predicateForContacts(matching: phoneNumber)
+        }
+        if let emailAddress = call.options["emailAddress"] as? String {
+            predicate = CNContact.predicateForContacts(matchingEmailAddress: emailAddress)
+        }
+        return predicate
     }
     
     private func getAllMocked() -> [Any] {
